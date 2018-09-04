392c2676ba7d1e47bb3d3d17ed134269e8e6cb38
Guest User <Guest@epicodus-50.local>
2018-09-03 19:58:48 -0700

    react hw

---
+++ src/components/TicketList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Ticket from "./Ticket";
+
+function TicketList() {
+  return(
+    <Ticket
+      location="G2"
+      names="Thato and Heley"
+      issue="Firebase not recording"
+      />
+  );
+}
+
+export default TicketList;
